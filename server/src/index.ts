edb78a6b2547f5ae3130f042f28aebdf25367b14
Preston Darrough <techdarrough@gmail.com>
2022-06-18 13:19:51 -0700

    expresss config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi')
+// Env vars and stripe api
+import { config } from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+    config();
+}
+
+// Init Stripe
+import Stripe from "stripe";
+export const stripe = new Stripe(process.env.STRIPE_SECERT, {
+    apiVersion: "2020-03-02",
+});
+
+import { app } from "./api";
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`API running on http://localhost:${port}`));
